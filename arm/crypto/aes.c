511624eec78f38a70f183da69404f538a7e50d7d
rw-r-r-0644 <rw-r-r-0644@protonmail.com>
2022-07-12 20:34:00 +0200

    crypto/aes.c: add missing copyright header

--- arm/crypto/aes.c
+++ arm/crypto/aes.c
@@ -1,3 +1,17 @@
+/*
+ *  minute - a port of the "mini" IOS replacement for the Wii U.
+ *
+ *  Copyright (C) 2016          SALT
+ *  Copyright (C) 2016          Daz Jones <daz@dazzozo.com>
+ *
+ *  Copyright (C) 2008, 2009    Haxx Enterprises <bushing@gmail.com>
+ *  Copyright (C) 2008, 2009    Sven Peter <svenpeter@gmail.com>
+ *  Copyright (C) 2008, 2009    Hector Martin "marcan" <marcan@marcansoft.com>
+ *
+ *  This code is licensed to you under the terms of the GNU GPL, version 2;
+ *  see file COPYING or http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
 #include "aes.h"
 #include "system/latte.h"
 #include "common/utils.h"
